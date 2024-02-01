1c21a6c7437dc638352c864f93c07d0fa7eb120e
D4nutz <manciudanielvlog1@gmail.com>
2024-02-01 15:15:14 +0200

    navbar

--- movie-app/src/main.ts
+++ movie-app/src/main.ts
@@ -4,3 +4,17 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("#search-icon")?.addEventListener("click", () => {
+      console.log('clicked');
+      document.querySelector(".nav")?.classList.toggle("search");
+      document.querySelector(".nav")?.classList.toggle("no-search");
+      document.querySelector(".search-input")?.classList.toggle("search-active");
+    });
+    
+    document.querySelector(".menu-toggle")?.addEventListener("click", () => {
+      document.querySelector(".nav")?.classList.toggle("mobile-nav");
+      document.querySelector(".menu-toggle")?.classList.toggle("is-active");
+    });
+  });
